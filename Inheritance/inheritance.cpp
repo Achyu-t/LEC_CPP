4f44327eb7d157409e3bf94047b0e7c32e196104
Achyu-t <acchyutbhattarai@gmail.com>
2023-07-04 20:26:33 +0545

    commit

--- Inheritance/inheritance.cpp
+++ Inheritance/inheritance.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 class Parent
 {
+    private:
+    int id_private;
     protected:
     int id_protected;
 };
@@ -23,6 +25,13 @@ int main()
 {
     Parent p1;
     Child c1;
+
+    //p1.id_protected=10;       //only derived class can access protected attributes(not even parent class).So the code causes error
+    //c1.id_private=2;         //Private attributes cannot be accessed by anyone outside class not even derived class.
+                            //thus this code also causes error.
+   
+   
+   
     c1.setId(2);
     c1.showdata();
     p1.id=5;
